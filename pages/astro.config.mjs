e0f612c58e104647aeb803bc75cef9d1754e6116
Alvaro Cervan <89271771+JackGraymer@users.noreply.github.com>
2026-01-26 00:03:19 +0100

    fix local images #1

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jackgraymer.github.io',
-  base: '/JackGraymer',
+  base: '', 
+  trailingSlash: 'always',
   outDir: 'public',
   publicDir: 'static', // Change source public dir to avoid conflict with outDir
   integrations: [
